deb9105eb552182ba1ee76c4c6e0b0a1c6f82fa5
arifmasyhur20 <arifmasyhur20@gmail.com>
2021-07-04 14:18:53 +0800

    Arif Start the UI for booking details

---
+++ lib/screens/staff/manage_booking/booking_details/booking_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingDetailsScreen extends StatelessWidget {
+  const BookingDetailsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/staff/manage_booking/booking_list/booking_list_screen.dart
+++ lib/screens/staff/manage_booking/booking_list/booking_list_screen.dart
@@ -1,10 +1,10 @@
 // import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 import 'package:restaurant_booking_system/screens/staff/manage_booking/booking_list/booking_list_body.dart';
 // import 'package:restaurant_booking_system/screens/staff/manage_booking/booking_list/booking_list_body.dart';
-import 'package:restaurant_booking_system/viewmodels/booking_list_viewmodel.dart';
+// import 'package:restaurant_booking_system/viewmodels/booking_list_viewmodel.dart';
 
 import 'booking_list_appbar.dart';
 
